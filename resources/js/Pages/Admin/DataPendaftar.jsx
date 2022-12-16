148dced505c804c8a0fcad0744fdf39ee8e7aae5
taqiussa <taqiussa@gmail.com>
2022-12-16 12:09:50 +0700

    table react

--- resources/js/Pages/Admin/DataPendaftar.jsx
+++ resources/js/Pages/Admin/DataPendaftar.jsx
@@ -1,14 +1,84 @@
 import AppLayout from '@/Layouts/AppLayout'
 import { Head } from '@inertiajs/inertia-react'
-import React from 'react'
+import React, { useState } from 'react'
+import Table from '../Table/Index'
 
 const DataPendaftar = ({ listPendaftar }) => {
+    const [rowData, setRowData] = useState([])
 
+    const onAddRowClick = () => {
+
+        setRowData(
+
+            rowData.concat({ username: "", email: "", gender: "", phone: "" })
+
+        )
+
+
+
+    }
+
+    const columns = [
+
+        {
+
+            Header: "Name",
+
+            accessor: "username",
+
+        },
+
+        {
+
+            Header: "Email",
+
+            accessor: "email",
+
+        },
+
+        {
+
+            Header: "Gender",
+
+            accessor: "gender",
+
+        },
+
+        {
+
+            Header: "Phone",
+
+            accessor: "phone",
+
+        },
+
+    ]
     return (
         <>
             <Head title='Data Pendaftar' />
             <div className="block border border-slate-300 p-4 shadow-md rounded-lg mb-5">
-                <div className="overflow-x-auto">
+                <div className="container mx-auto">
+
+                    <button
+
+                        onClick={onAddRowClick}
+
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+
+                    >
+
+                        Add Row
+
+                    </button>
+
+                    <div className="flex justify-center mt-8">
+
+                        <Table columns={columns} data={rowData} />
+
+                    </div>
+
+                </div>
+                {/* <div className="overflow-x-auto">
                     <table className=" border border-slate-500 border-separate w-full">
                         <thead>
                             <tr>
@@ -27,7 +97,7 @@ const DataPendaftar = ({ listPendaftar }) => {
                             ))}
                         </tbody>
                     </table>
-                </div>
+                </div> */}
             </div>
         </>
     )
